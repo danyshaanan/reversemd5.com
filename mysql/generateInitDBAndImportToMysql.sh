6d3fa8ee73514c58cc34180fb18275cd5d32fc73
Dany <danyshaanan@gmail.com>
2013-12-06 11:58:11 +0200

    updated cheatsheet

--- mysql/generateInitDBAndImportToMysql.sh
+++ mysql/generateInitDBAndImportToMysql.sh
@@ -1,12 +1,14 @@
 
-#local:
-python generateStrings.py.py > temp.sql
-tar vfcj temp.sql.tar.bz2 temp.sql
-scp temp.sql.tar.bz2 eg:
 
-#remote:
-mv ~/temp.sql.tar.bz2 .
-tar vfxj temp.sql.tar.bz2
-mysql md5 < init.sql
-mysql md5 < temp.sql
+python generateStrings.py > temp.sql
+tar cfvj temp.sql.tar.bz2 temp.sql
+scp temp.sql.tar.bz2 user@server:
+rm temp.sql
+
+ssh user@server
+
+tar xfv temp.sql.tar.bz2
+mysql DatabaseName < initDB.sql
+mysql DatabaseName < temp.sql
+rm temp.sql temp.sql.tar.bz2
 
