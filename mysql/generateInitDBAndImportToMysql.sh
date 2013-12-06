addbcdfa07b1d19eb90c4b537e310560a64c3ec2
Dany <danyshaanan@gmail.com>
2013-12-06 03:19:29 +0200

    renamed mysql script files

--- mysql/generateInitDBAndImportToMysql.sh
+++ mysql/generateInitDBAndImportToMysql.sh
@@ -1,10 +1,10 @@
 
-local:
-python md5.py > temp.sql
+#local:
+python generateStrings.py.py > temp.sql
 tar vfcj temp.sql.tar.bz2 temp.sql
 scp temp.sql.tar.bz2 eg:
 
-remote:
+#remote:
 mv ~/temp.sql.tar.bz2 .
 tar vfxj temp.sql.tar.bz2
 mysql md5 < init.sql
