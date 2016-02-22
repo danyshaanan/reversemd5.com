daafe50f976ee0794353288db9eba488f4f3247b
Dany Shaanan <danyshaanan@gmail.com>
2016-02-22 13:02:34 +0200

    Add logging

--- reversemd5.js
+++ reversemd5.js
@@ -12,6 +12,7 @@ app.use(express.static(__dirname + '/public/', { index: 'index.htm' }))
 
 app.get('/get', function(req, res) {
   var md5 = req.query.md5
+  console.log('get', md5)
   connection.query('SELECT * FROM md5 WHERE ?', { md5: md5 }, function(err, result) {
     if (err) throw new Error(err)
     res.send(result[0] && result[0].str)
@@ -20,6 +21,7 @@ app.get('/get', function(req, res) {
 
 app.get('/set', function(req, res) {
   var str = req.query.str || ''
+  console.log('set', str)
   var md5 = crypto.createHash('md5').update(str).digest('hex')
   connection.query('INSERT IGNORE INTO md5 set ?', { str: str, md5: md5 }, function(err, result) {
     if (err) throw new Error(err)
